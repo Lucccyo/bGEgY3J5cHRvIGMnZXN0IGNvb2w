bf53c1e1b8d797253267f9975c9c74e7dff52278
Lucccyo <cha.git@mailo.fr>
2023-01-12 21:49:03 +0100

    rewrite ex1

--- s1/e1/hex_to_base64.ml
+++ s1/e1/hex_to_base64.ml
@@ -1,38 +1,37 @@
-let b64  = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-let fill = "000000"
+let b64_to_char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
-let hex_char_to_int c = int_of_string ("0x" ^ (Char.escaped c))
-let char_to_int c = int_of_string ((Char.escaped c))
-let (<<) = Int.shift_left
-let (>>) = Int.shift_right
+let int_of_hex_char c =
+  let c = Char.code c in
+  let d = c - Char.code '0' in
+  if 0 <= d && d < 10 then d else
+  let d = c - Char.code 'A' in
+  if 0 <= d && d < 6 then d + 10 else
+  let d = c - Char.code 'a' in
+  if 0 <= d && d < 6 then d + 10 else
+  assert false
 
-let explode s =
-  let rec expl i l =
-    if i < 0 then l else
-    expl (i - 1) (s.[i] :: l) in
-  expl (String.length s - 1) []
+let bytes_of_hex_string hex_str =
+  let l = String.length hex_str in
+  if l mod 2 == 1 then assert false;
+  Bytes.init (l/2) (fun i -> Char.chr (
+    (int_of_hex_char hex_str.[2*i] lsl 4) lor (int_of_hex_char hex_str.[2*i+1])))
 
-let rec to_base64 buff cl =
-  match cl with
-  | [] -> buff
-  | c2 :: c1 :: c0:: tl -> 
-    let n = ((char_to_int c2) << 4) + ((char_to_int c1) << 2) + (char_to_int c0) in
-    to_base64 (buff ^ (Char.escaped b64.[n])) tl
-  | _ -> assert false
 
-let padd s =
-  let bit_size = 4 * (String.length s) in
-  let d = bit_size mod 6 in
-  let new_s = ref "" in
-  String.iter (fun c ->
-    let n = hex_char_to_int c in
-    new_s := !new_s ^ string_of_int (n >> 2) ^ string_of_int (n land 3))s;
-  if d = 0 then !new_s else !new_s ^ String.sub fill 0 ((6 - d)/2)
-
-let hex_to_base64 str =
-  to_base64 "" (explode(padd str))
+let b64_of_bytes bytes =
+  let blen = Bytes.length bytes in
+  let b64len = 4 * (blen / 3) + (match blen mod 3 with 0 -> 0 | 1 -> 2 | _ -> 3) in
+  let bget i = if i < blen then Char.code (Bytes.get bytes i) else 0 in
+  let b64s = String.init b64len (fun i ->
+    let deb = 3 * (i / 4) in
+    let b64 = match i mod 4 with
+      | 0 -> bget deb lsr 2
+      | 1 -> ((bget deb land 3) lsl 4) lor (bget (deb + 1) lsr 4)
+      | 2 -> ((bget (deb + 1) land 15) lsl 2) lor (bget (deb + 2) lsr 6)
+      | _ -> (bget (deb + 2)) land 63 in
+    b64_to_char.[b64]) in
+    b64s
 
 let () =
   let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d" in
-  let output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" in
-  Format.printf (if (hex_to_base64 input) = output then ":)\n" else ":(\n")
+  let bytes = bytes_of_hex_string input in
+  Format.printf "%s\n" (b64_of_bytes bytes)
