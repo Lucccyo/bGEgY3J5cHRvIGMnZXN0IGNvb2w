48af23f45d0b3f374a6f8d7c7ccab55cc3dd4287
Lucccyo <cha.git@mailo.fr>
2023-01-17 18:53:03 +0100

    score calculator might work

--- s1/e3/xor_cipher.ml
+++ s1/e3/xor_cipher.ml
@@ -1,26 +1,33 @@
-let ref = [|8.50; 2.07; 4.54; 3.38; 11.16; 1.81; 2.47; 3.01; 7.54; 0.20; 1.11; 5.49; 3.01; 6.65; 7.16; 3.16; 0.20; 7.58; 5.73; 6.95; 3.63; 1.01; 1.29; 0.29; 1.79; 0.27|]
-let sample = Array.map (fun f -> f /. 100.) ref
-(* Format.printf "%f\n" (Array.fold_left (fun acc item -> acc +. item) 0.0 ref) *)
+let src = [|8.50; 2.07; 4.54; 3.38; 11.16; 1.81; 2.47; 3.01; 7.54; 0.20; 1.11; 5.49; 3.01; 6.65; 7.16; 3.16; 0.20; 7.58; 5.73; 6.95; 3.63; 1.01; 1.29; 0.29; 1.79; 0.27|]
+let sample = Array.map (fun f -> f /. 100.) src
+(* Format.printf "%f\n" (Array.fold_left (fun acc item -> acc +. item) 0.0 src) *)
 
-let score_array bytes =
-  let score = Array.make (Array.length ref) 0. in
+let freq_array bytes =
+  let freqs = Array.make (Array.length src) 0. in
   let blf = Float.of_int (Bytes.length bytes) in
   Bytes.iter (fun c ->
     let code = Char.code c in
     let i = code - (if code <= 90 then 65 else 97) in
-    score.(i) <- score.(i) +. 1.
+    freqs.(i) <- freqs.(i) +. 1.
   ) bytes;
-  let score = Array.map (fun f -> f /. blf) score in
-  Array.iter (fun f -> Format.printf " %f " f) score
+  let freqs = Array.map (fun f -> f /. blf) freqs in
+  freqs
+  (* Array.iter (fun f -> Format.printf " %f " f) score *)
 
-
+let score freq_array =
+  let s = ref 0. in
+  Array.iter2 (fun freq src -> s := !s +. ((freq -. src)**2.)) freq_array src;
+  sqrt !s
 
 
 let () =
   (* let text = "hello" in *)
-  let byte_hello = Bytes.make 6 'a' in
-  Bytes.set byte_hello 1 'b';
-  score_array byte_hello
+  let byte_hello = Bytes.make 5 'l' in
+  Bytes.set byte_hello 0 'h';
+  Bytes.set byte_hello 1 'j';
+  Bytes.set byte_hello 4 'o';
+  let freq_array = freq_array byte_hello in
+  Format.printf "hzllo %f\n" (score freq_array)
 
 
 
