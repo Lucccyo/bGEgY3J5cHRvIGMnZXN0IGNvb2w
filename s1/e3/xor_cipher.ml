3cfb8789142073b7f672540c7903dde4adcecb19
Lucccyo <cha.git@mailo.fr>
2023-01-30 13:51:38 +0100

    Cooking MC's like a pound of bacon

--- s1/e3/xor_cipher.ml
+++ s1/e3/xor_cipher.ml
@@ -1,6 +1,11 @@
-let src = [|8.50; 2.07; 4.54; 3.38; 11.16; 1.81; 2.47; 3.01; 7.54; 0.20; 1.11; 5.49; 3.01; 6.65; 7.16; 3.16; 0.20; 7.58; 5.73; 6.95; 3.63; 1.01; 1.29; 0.29; 1.79; 0.27|]
+let src = [|0.0651738; 0.0124248; 0.0217339; 0.0349835; 0.1041442;
+            0.0197881; 0.0158610; 0.0492888; 0.0558094; 0.0009033;
+            0.0050529; 0.0331490; 0.0202124; 0.0564513; 0.0596302;
+            0.0137645; 0.0008606; 0.0497563; 0.0515760; 0.0729357;
+            0.0225134; 0.0082903; 0.0171272; 0.0013692; 0.0145984;
+            0.0007836; 0.1918182|]
+
 let sample = Array.map (fun f -> f /. 100.) src
-(* Format.printf "%f\n" (Array.fold_left (fun acc item -> acc +. item) 0.0 src) *)
 
 let freqs bytes =
   let freqs = Array.make (Array.length src) 0. in
@@ -8,38 +13,20 @@ let freqs bytes =
   Bytes.iter (fun c ->
     let code = Char.code c in
     match code with
-    (* | 32 -> () *)
+    | 32 -> freqs.(26) <- freqs.(26) +. 1.
     | n when (n < 91 && n > 64) || (n < 123 && n > 96) ->
-      if code = 32 then ();
       let i = code - (if code <= 90 then 65 else 97) in
       freqs.(i) <- freqs.(i) +. 1.
     | _ -> ()
   ) bytes;
   let freqs = Array.map (fun f -> f /. blf) freqs in
   freqs
-  (* Array.iter (fun f -> Format.printf " %f " f) score *)
 
 let score bytes =
-  let freq_array = freqs bytes in
-  let s = ref 0. in
-  Array.iter2 (fun freq src -> s := !s +. ((freq -. src)**2.)) freq_array src;
-  sqrt !s
-
-
-(* let () =
-  (* The higher the score, the more certain it is that it is an English text. *)
-  let byte_hello = Bytes.make 5 'l' in
-  Bytes.set byte_hello 0 'h';
-  Bytes.set byte_hello 1 'j';
-  Bytes.set byte_hello 4 'o';
-  let freq_array = freq_array byte_hello in
-  Format.printf "hjllo %f\n" (score freq_array) *)
-
-
-
-
-
-
+    let freq_array = freqs bytes in
+    let s = ref 0. in
+    Array.iter2 (fun freq src -> s := !s +. ((freq -. src)**2.)) freq_array src;
+    !s
 
 let int_of_hex_char c =
   let c = Char.code c in
@@ -61,21 +48,14 @@ let byte_to_int bytes i = Char.code (Bytes.get bytes i)
 
 let bytes_xor bytes n = Bytes.map (fun c -> Char.chr ((Char.code c) lxor n)) bytes
 
-let pp bytes = Bytes.iter (fun c -> Format.printf "%c " c) bytes; Format.printf "\n"
+let pp bytes = Bytes.iter (fun c -> Format.printf "%c " (if Char.code c > 31 && Char.code c < 127 then c else '?')) bytes; Format.printf "\n"
 
 let () =
-  let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b373630" in
-  (* I need to add 0 at the end to allow the bytes transformation *)
-    (*  let input = "48656C6C6F2C206D79206E616D6520697320686563746F72" in (* Hello, my name is Hector *)
-        let input = "65656565656565656565" in (* eeeeeeeeee *)
-        (* the second input is not english but more correct regarding score *)*)
+  let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736" in
   let bytes = bytes_of_hex_string input in
-
   let min_score_bytes = ref bytes in
-  for n = 0 to 255 do
+  for n = 0 to 127 do
     let xored = bytes_xor bytes n in
     if score xored < score !min_score_bytes then min_score_bytes := xored
   done;
   pp !min_score_bytes
-
-
